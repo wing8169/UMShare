066e4bf5edfd96ea07d4d4cb5df6a7e3595f7d04
CHIN JIA XIONG <chinxiongwei@yahoo.com.my>
2018-04-08 23:18:45 +0800

    bug fix

--- src/components/BeAStudent.vue
+++ src/components/BeAStudent.vue
@@ -28,18 +28,19 @@
       // get the user_info
       this.$firebase_basic.database().ref('users/' + this.uid).once('value').then((data)=> {
         this.user_info = data.val();
-      });
-      // grab the classes data and put in this.classes
-      this.$firebase_basic.database().ref('classes').on('value', (data)=> {
-        let tmp = data.val();
-        // if nothing then {}
-        if(!tmp) tmp = {};
-        for (let k in tmp) {
-          // push to the list-to-display if the key not exists in user_info.class
-          if (!this.user_info.class_learn.includes(tmp[k].key) && !this.user_info.class_teach.includes(tmp[k].key)) {
-            this.classes.push(tmp[k]);
+      }).then(()=>{
+        // grab the classes data and put in this.classes
+        this.$firebase_basic.database().ref('classes').on('value', (data)=> {
+          let tmp = data.val();
+          // if nothing then {}
+          if(!tmp) tmp = {};
+          for (let k in tmp) {
+            // push to the list-to-display if the key not exists in user_info.class
+            if (!this.user_info.class_learn.includes(tmp[k].key) && !this.user_info.class_teach.includes(tmp[k].key)) {
+              this.classes.push(tmp[k]);
+            }
           }
-        }
+        });
       });
     },
     methods:{
